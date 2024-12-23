50e5d0a4e010f679e6706c3ba70393d11d72c4e3
staks <stas.sor@gmail.com>
2024-12-23 20:15:16 +0300

    Refactor authentication exceptions into a common base class

--- app/api/v1/auth/schemas/exceptions.py
+++ app/api/v1/auth/schemas/exceptions.py
@@ -3,29 +3,38 @@ from core.enums.status_type import StatusType
 from core.enums.exception_status import ExceptionStatus
 
 
-class PasswordDoesNotMatchException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.PASSWORD_NOT_MATCHED.name
-    message = ExceptionStatus.PASSWORD_NOT_MATCHED.message
-    _status_code = ExceptionStatus.PASSWORD_NOT_MATCHED.status_code
+class BaseException(CustomException):
+    def __init__(self, exception_status: ExceptionStatus):
+        """
+        Базовый класс для всех пользовательских исключений.
+
+        :param exception_status: Статус исключения, определённый в `ExceptionStatus`
+        """
+        self.status = StatusType.ERROR.value
+        self.status_type = exception_status.name
+        self.message = exception_status.message
+        self._status_code = exception_status.status_code
 
 
-class InvalidTokenException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.INVALID_TOKEN.name
-    message = ExceptionStatus.INVALID_TOKEN.message
-    _status_code = ExceptionStatus.INVALID_TOKEN.status_code
+class PasswordDoesNotMatchException(BaseException):
+    def __init__(self):
+        # Инициализация для "PasswordDoesNotMatchException"
+        super().__init__(ExceptionStatus.PASSWORD_NOT_MATCHED)
 
 
-class DecodeTokenException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.UNPROCESSABLE_TOKEN.name
-    message = ExceptionStatus.UNPROCESSABLE_TOKEN.message
-    _status_code = ExceptionStatus.UNPROCESSABLE_TOKEN.status_code
+class InvalidTokenException(BaseException):
+    def __init__(self):
+        # Инициализация для "InvalidTokenException"
+        super().__init__(ExceptionStatus.INVALID_TOKEN)
 
 
-class ExpiredTokenException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.TOKEN_EXPIRED.name
-    message = ExceptionStatus.TOKEN_EXPIRED.message
-    _status_code = ExceptionStatus.TOKEN_EXPIRED.status_code
+class DecodeTokenException(BaseException):
+    def __init__(self):
+        # Инициализация для "DecodeTokenException"
+        super().__init__(ExceptionStatus.UNPROCESSABLE_TOKEN)
+
+
+class ExpiredTokenException(BaseException):
+    def __init__(self):
+        # Инициализация для "ExpiredTokenException"
+        super().__init__(ExceptionStatus.TOKEN_EXPIRED)
