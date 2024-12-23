7e7106904b695ba3a20ca579919afd96ad4032a3
staks <stas.sor@gmail.com>
2024-12-23 21:09:17 +0300

    user exceptions

--- app/api/v1/user/schemas/exceptions.py
+++ app/api/v1/user/schemas/exceptions.py
@@ -3,15 +3,21 @@ from core.enums.exception_status import ExceptionStatus
 from core.enums.status_type import StatusType
 
 
-class DuplicateEmailException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.DUPLICATE_EMAIL.name
-    message = ExceptionStatus.DUPLICATE_EMAIL.message
-    _status_code = ExceptionStatus.DUPLICATE_EMAIL.status_code
+class CustomStatusException(CustomException):
+    def __init__(self, exception_status: ExceptionStatus):
+        self.status = StatusType.ERROR.value
+        self.status_type = exception_status.name
+        self.message = exception_status.message
+        self._status_code = exception_status.status_code
 
 
-class UserNotFoundException(CustomException):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.USER_NOT_FOUND.name
-    message = ExceptionStatus.USER_NOT_FOUND.message
-    _status_code = ExceptionStatus.USER_NOT_FOUND.status_code
+# Исключение для дублирования email
+class DuplicateEmailException(CustomStatusException):
+    def __init__(self):
+        super().__init__(ExceptionStatus.DUPLICATE_EMAIL)
+
+
+# Исключение для случая, когда пользователь не найден
+class UserNotFoundException(CustomStatusException):
+    def __init__(self):
+        super().__init__(ExceptionStatus.USER_NOT_FOUND)
