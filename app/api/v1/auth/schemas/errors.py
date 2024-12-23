ab103405dbf01226d77e85db8548ad86ac969aff
staks <stas.sor@gmail.com>
2024-12-23 20:10:39 +0300

    Refactor authentication errors into a common base class

--- app/api/v1/auth/schemas/errors.py
+++ app/api/v1/auth/schemas/errors.py
@@ -3,29 +3,39 @@ from core.enums.status_type import StatusType
 from core.schemas.base import BaseAuthenticationError
 
 
-class CustomUnauthorizedError(BaseAuthenticationError):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.UNAUTHORIZED.name
-    message = ExceptionStatus.UNAUTHORIZED.message
-    _status_code = ExceptionStatus.UNAUTHORIZED.status_code
+class CustomAuthenticationError(BaseAuthenticationError):
+    def __init__(self, exception_status: ExceptionStatus):
+        """
+        Базовый класс для всех ошибок аутентификации.
+
+        :param exception_status: Статус ошибки, определённый в `ExceptionStatus`
+        """
+        self.status = StatusType.ERROR.value
+        self.status_type = exception_status.name
+        self.message = exception_status.message
+        self._status_code = exception_status.status_code
 
 
-class CustomInvalidTokenError(BaseAuthenticationError):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.INVALID_TOKEN.name
-    message = ExceptionStatus.INVALID_TOKEN.message
-    _status_code = ExceptionStatus.INVALID_TOKEN.status_code
+class CustomUnauthorizedError(CustomAuthenticationError):
+    def __init__(self):
+        # Инициализация конкретной ошибки "Unauthorized"
+        super().__init__(ExceptionStatus.UNAUTHORIZED)
 
 
-class CustomDecodeTokenError(BaseAuthenticationError):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.UNPROCESSABLE_TOKEN.name
-    message = ExceptionStatus.UNPROCESSABLE_TOKEN.message
-    _status_code = ExceptionStatus.UNPROCESSABLE_TOKEN.status_code
+class CustomInvalidTokenError(CustomAuthenticationError):
+    def __init__(self):
+        # Инициализация ошибки "Invalid Token"
+        super().__init__(ExceptionStatus.INVALID_TOKEN)
 
 
-class CustomExpiredTokenError(BaseAuthenticationError):
-    status = StatusType.ERROR.value
-    status_type = ExceptionStatus.TOKEN_EXPIRED.name
-    message = ExceptionStatus.TOKEN_EXPIRED.message
-    _status_code = ExceptionStatus.TOKEN_EXPIRED.status_code
+class CustomDecodeTokenError(CustomAuthenticationError):
+    def __init__(self):
+        # Инициализация ошибки "Unprocessable Token"
+        super().__init__(ExceptionStatus.UNPROCESSABLE_TOKEN)
+
+
+class CustomExpiredTokenError(CustomAuthenticationError):
+    def __init__(self):
+        # Инициализация ошибки "Token Expired"
+        super().__init__(ExceptionStatus.TOKEN_EXPIRED)
+
